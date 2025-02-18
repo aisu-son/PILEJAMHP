8f97bb5f97fa2311d084493bad1b7bea84dc486f
aisu-son <93694464+aisu-son@users.noreply.github.com>
2025-02-18 21:34:01 +0900

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW > 880){
+		var headerH =20;
+	}
+	else{
+		var headerH =60;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+
+    $('.panel').hide();
+
+    $('#menuWrap').on('click', function(){
+    $(this).next().slideToggle(); // 次の要素をスライドで開閉
+    $('#menuBtn').toggleClass('close'); // ボタンの見た目を変更
+    });
+
+});
